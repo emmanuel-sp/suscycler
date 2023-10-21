45fbad216587d07f2ac0985997955a0e450879fe
emmanuel-sp <esp48321@uga.edu>
2023-10-21 16:01:44 -0400

    Add spring commit

---
+++ suscycler/src/test/java/uga/hacks/suscycler/SuscyclerApplicationTests.java
@@ -0,0 +1,13 @@
+package uga.hacks.suscycler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuscyclerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ suscycler/src/uga/hacks/suscycler/SuscyclerApplication.java
@@ -0,0 +1,13 @@
+package uga.hacks.suscycler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuscyclerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuscyclerApplication.class, args);
+	}
+
+}
